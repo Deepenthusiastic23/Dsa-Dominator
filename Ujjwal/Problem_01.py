e69ec0d246a0c379b379e1f20a84a9fc5585f2af
Ujjwal Kumar <chotuujjwalkumar@gmail.com>
2026-07-15 19:12:50 +0530

    updated Problem_01 solution

--- Ujjwal/Problem_01.py
+++ Ujjwal/Problem_01.py
@@ -1,1 +1,11 @@
-print("Hello world")
+# Given a string s, reverse the string and return the reversed string.
+# You are not allowed to use any built-in reverse function.
+
+s = input("Enter your Text: ")
+
+reverse = ""
+
+for i in range(len(s) - 1, -1, -1):
+    reverse += s[i]
+
+print(reverse)
