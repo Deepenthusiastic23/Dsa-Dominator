11e4213c76d73ccd783e500fc47a21d96c98a275
Ujjwal Kumar <chotuujjwalkumar@gmail.com>
2026-07-15 22:24:53 +0530

    added 6.py solution

---
+++ Ujjwal/6.py
@@ -0,0 +1,11 @@
+'''Asked In: TCS Digital, Cognizant, Wipro
+Problem Statement
+Given a string s, remove all spaces from the string and return the modified string.
+You must remove:
+• Leading spaces
+• Trailing spaces
+• Spaces between words
+Do not remove any other characters.'''
+
+s = input("Enter your Text: ")
+print(s.replace(" ",""))
