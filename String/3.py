4c2d3f87c5cb67d4c6d685915d1e0b64582c9a8f
Deepenthusiastic23 <gdeepkumar9801@gmail.com>
2026-08-06 11:19:49 +0530

    Solved String Problem 3

--- String/3.py
+++ String/3.py
@@ -1,24 +1,39 @@
-'''
-Q3. Find the Length of a String Without Using len()
-Difficulty: Easy
-Asked In: Infosys, Accenture, Wipro
-Problem Statement:
-Given a string s, determine its length without using Python's built-in len() function.
-Input:
-A string s.
-Output:
-Return the total number of characters.
-Constraints
-• 1 <= length <= 10^5
+# '''
+# Q3. Find the Length of a String Without Using len()
+# Difficulty: Easy
+# Asked In: Infosys, Accenture, Wipro
+# Problem Statement:
+# Given a string s, determine its length without using Python's built-in len() function.
+# Input 
+# A string s. 
+ 
+# Output 
+# Return the total number of characters. 
+ 
+# Constraints 
+# • 1 <= length <= 10^5  
+ 
+# Example 1 
+# Input 
+# Python 
+# Output 
+# 6 
+ 
+# Example 2 
+# Input 
+# Interview 
+# Output 
+# 9 
+ 
+# Follow-up 
+# Can you solve it in O(n) time? 
+ 
 
-3 ways:
-using for and while loop and Recursion
-'''
+s = input("Enter a string: ")
 
-#loop
-s = input()
 count = 0
-for ch in s:
-    count = count + 1
 
-print (count)    
+for ch in s:
+  count += 1
+
+print(count)
